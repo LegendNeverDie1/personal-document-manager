77bc9d98af9c4fe3c25a40b1760a7e128ce84448
BhureshK <bhureshsoni7@gmail.com>
2026-05-12 17:47:03 +0530

    Updated Home Screen: category UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,98 @@
+import 'package:documentmanager/providers/category_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() {
+      Provider.of<CategoryProvider>(
+        context,
+        listen: false,
+      ).loadCategories();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    final provider = Provider.of<CategoryProvider>(context);
+
     return Scaffold(
+
       appBar: AppBar(
         title: const Text('Your Vault'),
       ),
-      body: const Center(
-        child: Text(
-          'Your Vault',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+
+      body: provider.categories.isEmpty
+          ? const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Icon(
+                    Icons.folder_open,
+                    size: 80,
+                    color: Colors.grey,
+                  ),
+
+                  SizedBox(height: 16),
+
+                  Text(
+                    'No folders yet',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 8),
+
+                  Text(
+                    'Create your first vault category',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            )
+
+          : ListView.builder(
+              itemCount: provider.categories.length,
+
+              itemBuilder: (context, index) {
+
+                final category = provider.categories[index];
+
+                return ListTile(
+
+                  leading: const Icon(Icons.folder),
+
+                  title: Text(category.name),
+
+                  subtitle: Text(
+                    category.createdAt.toString(),
+                  ),
+                );
+              },
+            ),
+
+      floatingActionButton: FloatingActionButton(
+
+        onPressed: () {},
+
+        child: const Icon(Icons.add),
       ),
     );
   }
